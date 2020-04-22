233b63aec6cda3e37cc3695811e1e51a65db3873
Jason Barr <me@jasonbarr.dev>
2020-04-22 17:42:36 -0500

    Emit JavaScript file for integer literal

--- src/parse-and-evaluate.js
+++ src/parse-and-evaluate.js
@@ -2,7 +2,16 @@ const { pipe } = require("./utilities");
 const { tokenize } = require("./tokenize");
 const { parse } = require("./parse");
 const { evaluate } = require("./evaluate");
+const { transpile } = require("./transpile");
 
 const parseAndEvaluate = pipe(tokenize, parse, evaluate);
 
-module.exports = { parseAndEvaluate };
+const tokenizeAndParse = pipe(tokenize, parse);
+
+const parseAndTranspile = pipe(tokenizeAndParse, transpile);
+
+module.exports = {
+  parseAndEvaluate,
+  tokenizeAndParse,
+  parseAndTranspile,
+};

ec1b54336717d411cfd211da4235863fe0b5c4ac
Jason Barr <me@jasonbarr.dev>
2020-04-22 17:43:48 -0500

    Use util

--- src/parse-and-evaluate.js
+++ src/parse-and-evaluate.js
@@ -4,14 +4,13 @@ const { parse } = require("./parse");
 const { evaluate } = require("./evaluate");
 const { transpile } = require("./transpile");
 
-const parseAndEvaluate = pipe(tokenize, parse, evaluate);
-
 const tokenizeAndParse = pipe(tokenize, parse);
+
+const parseAndEvaluate = pipe(tokenizeAndParse, evaluate);
 
 const parseAndTranspile = pipe(tokenizeAndParse, transpile);
 
 module.exports = {
   parseAndEvaluate,
-  tokenizeAndParse,
   parseAndTranspile,
 };

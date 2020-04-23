8799a1fed5514b416ec08e91e38d7812a10bcb5f
Jason Barr <me@jasonbarr.dev>
2020-04-22 19:46:46 -0500

    Add whitespace identifier

--- src/identifiers.js
+++ src/identifiers.js
@@ -1,7 +1,10 @@
 // constants
 const INTEGER = /^[0-9]+$/;
+const WHITESPACE = /\s+/;
 
 // token identifiers
 const isInteger = (char) => INTEGER.test(char);
 
-module.exports = { isInteger };
+const isWhitespace = (char) => WHITESPACE.test(char);
+
+module.exports = { isInteger, isWhitespace };

c88278b1eb0e4b6ab477300dcdb5f433fbbdf4b0
Jason Barr <me@jasonbarr.dev>
2020-04-24 10:08:47 -0500

    Lookup identifiers and apply function calls

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,4 +1,35 @@
+const stdlib = require("./stdlib");
+
+const environment = { ...stdlib };
+
+const getIdentifier = (node) => {
+  if (environment[node.name]) {
+    return environment[node.name];
+  }
+
+  throw new ReferenceError(`${node.name} is not defined`);
+};
+
+const apply = (node) => {
+  const fn = environment[node.name];
+  const args = node.arguments.map(evaluate);
+
+  if (typeof fn !== "function") {
+    throw new TypeError(`${node.name} is not a function`);
+  }
+
+  return fn(...args);
+};
+
 const evaluate = (node) => {
+  switch (node.type) {
+    case "Identifier":
+      return getIdentifier(node);
+
+    case "CallExpression":
+      return apply(node);
+  }
+
   if (node.value) {
     return node.value;
   } else if (node.value === 0) {

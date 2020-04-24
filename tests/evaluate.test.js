9631e96384bc255f82db9a80d2e9dcc961e1249c
Jason Barr <me@jasonbarr.dev>
2020-04-24 10:16:05 -0500

    Add tests for evaluator

--- tests/evaluate.test.js
+++ tests/evaluate.test.js
@@ -18,4 +18,56 @@ describe("AST Evaluator", () => {
 
     expect(evaluate(ast)).toBe(3.14159);
   });
+
+  it("Should be able to look up identifiers in the environment", () => {
+    const ast = { type: "Identifier", name: "PI" };
+    expect(evaluate(ast)).toBe(Math.PI);
+  });
+
+  it("Should be able to evaluate a single call expression", () => {
+    const ast = {
+      type: "CallExpression",
+      name: "add",
+      arguments: [
+        { type: "IntegerLiteral", value: 2 },
+        { type: "IntegerLiteral", value: 3 },
+      ],
+    };
+
+    expect(evaluate(ast)).toBe(5);
+  });
+
+  it("Should be able to evaluate a call expression with float arguments", () => {
+    const ast = {
+      type: "CallExpression",
+      name: "sub",
+      arguments: [
+        { type: "FloatLiteral", value: 6.4 },
+        { type: "FloatLiteral", value: 3.2 },
+      ],
+    };
+
+    expect(evaluate(ast)).toBeCloseTo(3.2);
+  });
+
+  it("Should be able to evaluate a nested expression", () => {
+    const ast = {
+      type: "CallExpression",
+      name: "add",
+      arguments: [
+        { type: "IntegerLiteral", value: 2 },
+        { type: "IntegerLiteral", value: 3 },
+        {
+          type: "CallExpression",
+          name: "sub",
+          arguments: [
+            { type: "IntegerLiteral", value: 5 },
+            { type: "IntegerLiteral", value: 4 },
+          ],
+        },
+      ],
+    };
+
+    expect(evaluate(ast)).toBe(6);
+  });
 });

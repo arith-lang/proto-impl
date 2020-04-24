1c60180f975c68b4e690040ea7759475ce4806cc
Jason Barr <me@jasonbarr.dev>
2020-04-24 11:03:57 -0500

    Transpile call expressions

--- src/transpile.js
+++ src/transpile.js
@@ -1,4 +1,5 @@
 const transpile = (node) => {
+  console.log(node.type);
   return (
     emit[node.type](node) ||
     new Error(`Cannot generate code for ${node.type}`)
@@ -9,9 +10,26 @@ const IntegerLiteral = ({ value }) => `${value}`;
 
 const FloatLiteral = ({ value }) => `${value}`;
 
+const Identifier = ({ name }) => `${name}`;
+
+const CallExpression = (node) => {
+  let code = node.arguments.reduce((acc, c, i, a) => {
+    let tmp = acc + transpile(c);
+    if (i + 1 < a.length) tmp += ", ";
+
+    return tmp;
+  }, `${node.name}(`);
+
+  code += ");";
+
+  return code;
+};
+
 const emit = {
   IntegerLiteral,
   FloatLiteral,
+  Identifier,
+  CallExpression,
 };
 
 module.exports = { transpile };

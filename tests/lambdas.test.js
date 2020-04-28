60f5c1a88f0539de251873b9ea6036a65db8bd8c
Jason Barr <me@jasonbarr.dev>
2020-04-28 09:07:06 -0500

    Test defining variables with lambda expression values

--- tests/lambdas.test.js
+++ tests/lambdas.test.js
@@ -74,12 +74,82 @@ describe("Lambda expression tests", () => {
     expect(parse(tokens)).toEqual(ast);
   });
 
+  it("Should correctly parse a definition expression with a lambda value", () => {
+    const tokens = [
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "define" },
+      { type: "IDENTIFIER", value: "add2" },
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "lambda" },
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "x" },
+      { type: "IDENTIFIER", value: "y" },
+      { type: "PAREN", value: ")" },
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "+" },
+      { type: "IDENTIFIER", value: "x" },
+      { type: "IDENTIFIER", value: "y" },
+      { type: "PAREN", value: ")" },
+      { type: "PAREN", value: ")" },
+      { type: "PAREN", value: ")" },
+    ];
+
+    const ast = {
+      type: "DefinitionExpression",
+      name: "add2",
+      value: {
+        type: "LambdaExpression",
+        params: [
+          { type: "FunctionParameter", name: "x" },
+          { type: "FunctionParameter", name: "y" },
+        ],
+        body: {
+          type: "CallExpression",
+          name: "+",
+          arguments: [
+            { type: "Identifier", name: "x" },
+            { type: "Identifier", name: "y" },
+          ],
+        },
+      },
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+  });
+
+  it("Should correctly parse defining the identity function", () => {
+    const tokens = [
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "define" },
+      { type: "IDENTIFIER", value: "identity" },
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "lambda" },
+      { type: "PAREN", value: "(" },
+      { type: "IDENTIFIER", value: "x" },
+      { type: "PAREN", value: ")" },
+      { type: "IDENTIFIER", value: "x" },
+      { type: "PAREN", value: ")" },
+      { type: "PAREN", value: ")" },
+    ];
+
+    const ast = {
+      type: "DefinitionExpression",
+      name: "identity",
+      value: {
+        type: "LambdaExpression",
+        params: [{ type: "FunctionParameter", name: "x" }],
+        body: { type: "Identifier", name: "x" },
+      },
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+  });
+
   it.skip("Should correctly evaluate a call expression invoking a lambda", () => {
     const ast = {
       type: "CallExpression",
       lambda: {
         type: "LambdaExpression",
-        name: "lambda2",
         params: [{ type: "FunctionParameter", name: "x" }],
         body: {
           type: "CallExpression",
@@ -90,7 +160,7 @@ describe("Lambda expression tests", () => {
           ],
         },
       },
-      name: "lambda2",
+      name: "lambda",
       arguments: [{ type: "IntegerLiteral", value: 10 }],
     };
 

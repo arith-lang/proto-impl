ea4d0da615116de8e22eb9372a72b562a6b702ba
Jason Barr <me@jasonbarr.dev>
2020-05-11 16:46:56 -0500

    Add string and file i/o primitives

--- src/stdlib/io.js
+++ src/stdlib/io.js
@@ -1,8 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const readlineSync = require("readline-sync");
+const { list } = require("./list");
+const toString = require("./list")["to-string"];
+const isList = require("./list")["list?"];
+
 // I/O functions
 function log(...args) {
   console.log(...args);
 }
 
+function print(...args) {
+  let temp = [];
+  for (item of args) {
+    if (isList(item)) {
+      temp.push(toString(item));
+    } else {
+      temp.push(item.toString());
+    }
+  }
+  console.log(...temp);
+}
+
+const outputString = print;
+
+function input(prompt) {
+  return readlineSync.question(prompt);
+}
+
+const inputString = input;
+
+// Working with files
+// Encoding only necessary if text file
+function inputFileRead(file, encoding) {
+  const realPath = /^\./.test(file)
+    ? path.join(process.cwd(), file)
+    : file;
+  return fs.readFileSync(realPath, encoding);
+}
+
+function outputFileWrite(file, data, encoding) {
+  const realPath = /^\./.test(file)
+    ? path.join(process.cwd(), file)
+    : file;
+  fs.writeFileSync(realPath, data, encoding);
+}
+
 module.exports = {
   log,
+  print,
+  "output-string": outputString,
+  input,
+  "input-string": input,
+  "input-file-read": inputFileRead,
+  "output-file-write": outputFileWrite,
 };

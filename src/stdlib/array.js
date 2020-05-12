91d5ee69b9b92af6c27c42ea8cc20b72c16bf66c
Jason Barr <me@jasonbarr.dev>
2020-05-12 13:40:01 -0500

    Add array helper functions

--- src/stdlib/array.js
+++ src/stdlib/array.js
@@ -49,6 +49,37 @@ function arrayLength(arr) {
   return arr.length;
 }
 
+function arraySlice(start, end, arr) {
+  return array(...arr.slice(start, end));
+}
+
+function arrayPrepend(item, arr) {
+  arr.unshift(item);
+}
+
+function arrayAppend(item, arr) {
+  arr.push(item);
+}
+
+function arrayConcat(...arrs) {
+  let accum = [];
+  return array(...accum.concat(...arrs)).toString();
+}
+
+function arrayCopy(arr) {
+  return array(...arr);
+}
+
+function arrayReverse(arr) {
+  arr.reverse();
+}
+
+function vectorSet(pos, newItem, arr) {
+  arr.splice(pos, 1, newItem);
+}
+
+const vectorUpdate = vectorSet;
+
 // array iterators
 
 // conversion functions
@@ -66,4 +97,11 @@ module.exports = {
   "array?": isArray,
   "array-empty?": isArrayEmpty,
   "array-length": arrayLength,
+  "array-slice": arraySlice,
+  "array-prepend!": arrayPrepend,
+  "array-append!": arrayAppend,
+  "array-concat": arrayConcat,
+  "array-reverse!": arrayReverse,
+  "vector-update!": vectorUpdate,
+  "vector-set!": vectorSet,
 };

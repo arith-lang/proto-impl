d4ce1bb4ae4876644a5478b3700cfdca25e2c4c5
Jason Barr <me@jasonbarr.dev>
2020-05-13 12:25:40 -0500

    Add lookahead utility

--- src/utilities.js
+++ src/utilities.js
@@ -1,8 +1,9 @@
 const peek = (sequence) => sequence[0];
+const lookahead = (pos, sequence) => sequence[pos];
 const pop = (sequence) => sequence.shift();
 const push = (token, sequence) => sequence.unshift(token);
 
 const pipe = (...funcs) => (value) =>
   funcs.reduce((value, func) => func(value), value);
 
-module.exports = { peek, pop, push, pipe };
+module.exports = { peek, lookahead, pop, push, pipe };

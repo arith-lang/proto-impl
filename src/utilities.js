715933a17fce4f1d49b6b8cd1309119195285033
Jason Barr <me@jasonbarr.dev>
2020-05-13 11:55:16 -0500

    Add push utility function

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,8 @@
 const peek = (sequence) => sequence[0];
 const pop = (sequence) => sequence.shift();
+const push = (token, sequence) => sequence.unshift(token);
 
 const pipe = (...funcs) => (value) =>
   funcs.reduce((value, func) => func(value), value);
 
-module.exports = { peek, pop, pipe };
+module.exports = { peek, pop, push, pipe };

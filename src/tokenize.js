9f839cd5010ca43b048f4c897c3049f8150c6953
Jason Barr <me@jasonbarr.dev>
2020-05-13 18:46:16 -0500

    Rewrite input stream handler as function

--- src/tokenize.js
+++ src/tokenize.js
@@ -15,120 +15,149 @@ const {
   isSemicolon,
   isEndOfLine,
 } = require("./identifiers");
+const { peek } = require("./utilities");
+const { ArithReadInputError } = require("./errors");
 
-const tokenize = (input) => {
-  const tokens = [];
-  let i = 0;
+const getInputStream = (input) => {
+  let pos = 0;
+  let line = 1;
+  let col = 0;
 
-  while (i < input.length) {
-    const current = input[i];
-    // ; starts a comment - skip to end of line
-    if (isSemicolon(current)) {
-      while (!isEndOfLine(input[i])) {
-        i += 1;
-      }
-      continue;
-    }
-
-    // skip whitespace
-    if (isWhitespace(current)) {
-      i += 1;
-      continue;
-
-      // read numeric tokens
-    } else if (isInteger(current)) {
-      let value = current;
-      let j = 1;
-
-      // get the whole string of characters starting with the first int
-      while (
-        !isSeparator(input[i + j]) &&
-        !isEndOfInput(input, i + j)
-      ) {
-        value += input[i + j];
-        j += 1;
-      }
-
-      if (isInteger(value)) {
-        tokens.push(createIntegerToken(value));
-      } else if (isFloat(value)) {
-        tokens.push(createFloatToken(value));
-      } else {
-        throw new SyntaxError(`Invalid identifier ${value}`);
-      }
-
-      i += j;
-      continue;
-
-      // valid first character for an identifier
-    } else if (isLetter(current) || isValidSpecialChar(current)) {
-      // check for valid identifier token
-      let j = 1;
-      let value = current;
-
-      while (
-        !isSeparator(input[i + j]) &&
-        !isEndOfInput(input, i + j)
-      ) {
-        value += input[i + j];
-
-        if (
-          !isLetter(input[i + j]) &&
-          !isInteger(input[i + j]) &&
-          !isValidSpecialChar(input[i + j])
-        ) {
-          throw new SyntaxError(`${value} is not a valid identifier`);
-        }
-        j += 1;
-      }
-
-      if (value === "true" || value === "false") {
-        tokens.push(createBooleanToken(value));
-      } else {
-        tokens.push(createIdentifierToken(value));
-      }
-
-      i += j;
-      continue;
-
-      // parenthesis tokens for call expressions
-    } else if (isParen(current)) {
-      tokens.push(createParenToken(current));
-
-      // detect string literals and tokenize
-    } else if (isQuote(current)) {
-      i += 1;
-      let value = "";
-
-      while (!isQuote(input[i])) {
-        value += input[i];
-        i += 1;
-      }
-
-      tokens.push(createStringToken(value));
+  const next = () => {
+    let ch = input.charAt(pos);
+    if (ch == "\n") {
+      line++;
+      col = 0;
     } else {
-      // Is not a valid token type
-      throw new SyntaxError(`${current} is not a valid token`);
+      col++;
     }
+    pos++;
+    return ch;
+  };
 
-    i += 1;
-  }
+  const eof = () => {
+    return peek(input[pos]) === undefined;
+  };
 
-  return tokens;
-};
+  const die = (msg) => {
+    throw new ArithReadInputError(
+      `${msg} at line ${line}, col ${col}`,
+    );
+  };
 
-// token creator helpers
-const createIntegerToken = (value) => {
   return {
-    type: "INTEGER",
-    value: parseInt(value),
+    next,
+    eof,
+    die,
   };
 };
 
-const createFloatToken = (value) => {
+// const tokenize = (input) => {
+//   const tokens = [];
+//   let i = 0;
+
+//   while (i < input.length) {
+//     const current = input[i];
+//     // ; starts a comment - skip to end of line
+//     if (isSemicolon(current)) {
+//       while (!isEndOfLine(input[i])) {
+//         i += 1;
+//       }
+//       continue;
+//     }
+
+//     // skip whitespace
+//     if (isWhitespace(current)) {
+//       i += 1;
+//       continue;
+
+//       // read numeric tokens
+//     } else if (isInteger(current)) {
+//       let value = current;
+//       let j = 1;
+
+//       // get the whole string of characters starting with the first int
+//       while (
+//         !isSeparator(input[i + j]) &&
+//         !isEndOfInput(input, i + j)
+//       ) {
+//         value += input[i + j];
+//         j += 1;
+//       }
+
+//       if (isInteger(value)) {
+//         tokens.push(createIntegerToken(value));
+//       } else if (isFloat(value)) {
+//         tokens.push(createFloatToken(value));
+//       } else {
+//         throw new SyntaxError(`Invalid identifier ${value}`);
+//       }
+
+//       i += j;
+//       continue;
+
+//       // valid first character for an identifier
+//     } else if (isLetter(current) || isValidSpecialChar(current)) {
+//       // check for valid identifier token
+//       let j = 1;
+//       let value = current;
+
+//       while (
+//         !isSeparator(input[i + j]) &&
+//         !isEndOfInput(input, i + j)
+//       ) {
+//         value += input[i + j];
+
+//         if (
+//           !isLetter(input[i + j]) &&
+//           !isInteger(input[i + j]) &&
+//           !isValidSpecialChar(input[i + j])
+//         ) {
+//           throw new SyntaxError(`${value} is not a valid identifier`);
+//         }
+//         j += 1;
+//       }
+
+//       if (value === "true" || value === "false") {
+//         tokens.push(createBooleanToken(value));
+//       } else {
+//         tokens.push(createIdentifierToken(value));
+//       }
+
+//       i += j;
+//       continue;
+
+//       // parenthesis tokens for call expressions
+//     } else if (isParen(current)) {
+//       tokens.push(createParenToken(current));
+
+//       // detect string literals and tokenize
+//     } else if (isQuote(current)) {
+//       i += 1;
+//       let value = "";
+
+//       while (!isQuote(input[i])) {
+//         value += input[i];
+//         i += 1;
+//       }
+
+//       tokens.push(createStringToken(value));
+//     } else {
+//       // Is not a valid token type
+//       throw new SyntaxError(`${current} is not a valid token`);
+//     }
+
+//     i += 1;
+//   }
+
+//   return tokens;
+// };
+
+// token creator helpers
+const createNumberToken = (value) => {
   return {
-    type: "FLOAT",
-    value: parseFloat(value),
+    type: "Number",
+    value,
   };
 };
 
@@ -150,13 +179,6 @@ const createStringToken = (value) => {
   return {
     type: "STRING",
     value,
-  };
-};
-
-const createBooleanToken = (value) => {
-  return {
-    type: "BOOLEAN",
-    value: value === "true",
   };
 };
 

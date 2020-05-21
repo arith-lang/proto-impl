f5b25ff30c90d2a3f1cb7fd23e9c333a263d21c3
Jason Barr <me@jasonbarr.dev>
2020-05-21 12:17:36 -0500

    Add properties and methods to String class

--- src/stdlib/types/String.js
+++ src/stdlib/types/String.js
@@ -1,3 +1,33 @@
-class _String extends String {}
+const Char = require("./Char");
+const { array } = require("../array");
+const { vector } = require("../vector");
+const { list } = require("../list");
+
+class _String extends String {
+  constructor(string) {
+    super(string);
+    this.chars = Object.freeze(
+      string.split("").map((c) => new Char(c)),
+    );
+  }
+
+  toCharString() {
+    return this.chars.reduce((s, c) => s + c.value, "");
+  }
+
+  toCharArray() {
+    return array(...this.chars.map((c) => c.value));
+  }
+
+  toCharVector() {
+    return vector(...this.chars.map((c) => c.value));
+  }
+
+  toCharList() {
+    return list(...this.chars.map((c) => c.value));
+  }
+}
 
 module.exports = _String;
+
+console.log(new _String("☃★♲").toCharList());

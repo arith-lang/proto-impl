c988c204aae63e9095809fb640f3c24d4f6685ab
Jason Barr <me@jasonbarr.dev>
2020-05-22 13:51:42 -0500

    Add static helpers to _Boolean

--- src/stdlib/types/Boolean.js
+++ src/stdlib/types/Boolean.js
@@ -1,9 +1,37 @@
+const isNull = require("../list")["null?"];
+
 class _Boolean extends Boolean {
   constructor(value) {
-    const bool = value === "#f" ? false : true;
+    const bool = _Boolean.shouldReturnFalse(value) ? false : true;
     super(bool);
     this.value = value;
     this.bool = bool;
+  }
+
+  static make(value) {
+    return _Boolean.shouldReturnFalse(value)
+      ? new _Boolean("#f")
+      : new _Boolean("#t");
+  }
+
+  static isBoolNative(obj) {
+    return obj.constructor.name === "_Boolean";
+  }
+
+  static shouldReturnFalse(value) {
+    return (
+      value === false ||
+      value === "#f" ||
+      value == undefined ||
+      isNull(value) ||
+      (_Boolean.isBool(value) && value.bool === false)
+    );
+  }
+
+  static isBool(obj) {
+    return obj.constructor.name === "_Boolean"
+      ? new _Boolean("#t")
+      : new _Boolean("#f");
   }
 
   toString() {

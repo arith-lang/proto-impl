d46c680a0eb3ed3d446663c1b0531e65ad1cdf90
Jason Barr <me@jasonbarr.dev>
2020-05-27 09:52:10 -0500

    Split chars by grapheme

--- src/stdlib/types/Char.js
+++ src/stdlib/types/Char.js
@@ -6,12 +6,12 @@ class Char extends _Object {
   constructor(char) {
     super();
 
-    if (char.length > 1) {
+    if (v.countGraphemes(char) > 1) {
       throw new ArithTypeError("Char can only be a single character");
     }
 
     this.value = char;
-    this.code = char.codePointAt(0).toString(16).padStart(4, "0");
+    this.code = v.codePointAt(char, 0).toString(16).padStart(4, "0");
   }
 
   static isChar(obj) {

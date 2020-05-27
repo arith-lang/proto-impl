7fd92dae34f531f68ff9ff0f997a4b9b7abfbdbe
Jason Barr <me@jasonbarr.dev>
2020-05-27 14:37:02 -0500

    Add 2nd hash-table constructor

--- src/stdlib/hash.js
+++ src/stdlib/hash.js
@@ -3,8 +3,36 @@ const R = require("ramda");
 const { cons } = require("./list");
 
 // hash table constructor
+// immutable equivalent of hash maps
 // takes a sequence of cons pairs
 // do not curry
 function hash(...args) {
   return Map(args);
 }
+
+// second constructor
+// takes a sequence of values
+// do not curry
+function makeHash(...args) {
+  let hash = Map();
+  for (let i = 0; i < args.length; i += 2) {
+    hash = hash.set(args[i], args[i + 1]);
+  }
+
+  return hash;
+}
+
+// hash?
+
+// hash-eq?
+
+// hash-eqv?
+
+// hash-equals?
+
+// hash-ref
+
+module.exports = {
+  hash,
+  "make-hash": makeHash,
+};

31d4172260e530641fcdb6eb8fac8706be3ea164
Jason Barr <me@jasonbarr.dev>
2020-05-27 14:37:35 -0500

    Add 2nd hash map constructor

--- src/stdlib/map.js
+++ src/stdlib/map.js
@@ -5,6 +5,33 @@ const { cons } = require("./list");
 // mutable equivalent of hash tables
 // takes a sequence of cons pairs
 // do not curry
-function map(...args) {
+function hashmap(...args) {
   return new Map(args);
 }
+
+// second constructor
+// takes a sequence of values
+// do not curry
+function makeMap(...args) {
+  let map = new Map();
+  for (let i = 0; i < args.length; i += 2) {
+    map.set(args[i], args[i + 1]);
+  }
+
+  return map;
+}
+
+// map?
+
+// map-eq?
+
+// map-eqv?
+
+// map-equals?
+
+// map-ref
+
+module.exports = {
+  hashmap,
+  "make-map": makeMap,
+};

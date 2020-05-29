1e45ba8bfb9d0cf0b69f9de36bc699f6d29fd915
Jason Barr <me@jasonbarr.dev>
2020-05-29 12:44:11 -0500

    Define struct functions

--- src/stdlib/struct.js
+++ src/stdlib/struct.js
@@ -1,9 +1,24 @@
 const { Record } = require("immutable");
 
-function struct(obj) {
-  return Record(obj);
+function struct(obj, name) {
+  return Record(obj, name);
+}
+
+function getStructName(obj) {
+  return Record.getDescriptiveName(obj);
+}
+
+function getStructField(field, struct) {
+  return struct.get(field);
+}
+
+function setStructField(field, value, struct) {
+  return struct.set(field, value);
 }
 
 module.exports = {
   struct,
+  "get-struct-name": getStructName,
+  "get-struct-field": getStructField,
+  "set-struct-field": setStructField,
 };

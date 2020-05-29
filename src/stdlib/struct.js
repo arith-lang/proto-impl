601e7f22806461bdcf5fa3e171b9e21f87509d6f
Jason Barr <me@jasonbarr.dev>
2020-05-29 13:23:37 -0500

    Define struct-equal?

--- src/stdlib/struct.js
+++ src/stdlib/struct.js
@@ -16,10 +16,15 @@ function setStructField(field, value, struct) {
   return struct.set(field, value);
 }
 
+function isStructEqual(struct1, struct2) {
+  return struct1.equals(struct2);
+}
+
 module.exports = {
   struct,
   "get-struct-name": getStructName,
   "get-struct-field": getStructField,
   "set-struct-field": setStructField,
   "struct-update": setStructField,
+  "struct-equal?": isStructEqual,
 };

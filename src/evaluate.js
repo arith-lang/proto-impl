6d946f09539ceb98fb0627298059f167928e1a87
Jason Barr <me@jasonbarr.dev>
2020-05-29 13:21:09 -0500

    Fix struct constructor definition and define accessors

--- src/evaluate.js
+++ src/evaluate.js
@@ -8,7 +8,7 @@ const {
   getValue,
   defVar,
 } = require("./environment");
-const { ArithTypeError } = require("./errors");
+const { ArithTypeError, ArithReferenceError } = require("./errors");
 
 const globalEnv = setEnv(globals);
 const moduleEnv = createEnv(globalEnv);
@@ -87,14 +87,47 @@ const applyIf = (node, env) => {
 };
 
 const defineStruct = (node, env) => {
-  const obj = {};
-  node.fields.forEach((field) => (obj[field.name] = null));
-  defVar(node.name, globals.struct(obj, node.name), env);
+  const structDefiner = {};
+  node.fields.forEach((field) => (structDefiner[field.name] = null));
+  // defVar(node.name, globals.struct(obj, node.name), env);
+  const structConstructor = (...args) => {
+    const structFunc = globals["struct"](structDefiner, node.name);
+    let obj = {};
+    Object.keys(structDefiner).forEach((key, i) => {
+      if (!args[i]) {
+        throw new ArithReferenceError(
+          `Invalid number of arguments for struct constructor ${node.name}`,
+        );
+      }
+      obj[key] = args[i];
+    });
+    return structFunc(obj);
+  };
+  defVar(node.name, structConstructor, env);
 
   const pred = (obj) => {
     return globals["get-struct-name"](obj) === node.name;
   };
   defVar(`${node.name}?`, pred, env);
+
+  node.fields.forEach((field) => {
+    let accessor = (...args) => {
+      if (args.length === 1) {
+        return globals["get-struct-field"](field.name, args[0]);
+      } else if (args.length === 2) {
+        return globals["set-struct-field"](
+          field.name,
+          args[0],
+          args[1],
+        );
+      }
+      throw new ArithReferenceError(
+        `Invalid accessor call for struct ${node.name}`,
+      );
+    };
+    defVar(`${node.name}-${field.name}`, accessor, env);
+    console.log(env);
+  });
 };
 
 module.exports = { evaluate: (input) => evaluate(parse(input)) };

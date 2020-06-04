494f3d2369786fc94f1ee3bd5b7f3b7075c3bed1
Amy Shackles <amyshackles@gmail.com>
2020-06-04 13:26:42 -0700

    Added ability to quit repl and pull up help options

--- src/repl.js
+++ src/repl.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { prompt } = require("inquirer");
+const readline = require("readline");
 const chalk = require("chalk");
 const { evaluate } = require("./evaluate");
 
@@ -8,31 +8,118 @@ const version = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../package.json"), "utf-8"),
 ).version;
 
-const getInput = () => {
-  const input = [
-    {
-      name: "INPUT",
-      type: "input",
-      prefix: chalk.green(`(arith v${version}):`),
-      message: ">",
-    },
-  ];
+const repl = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: `${chalk.green(`(arith v${version}):`)} ${chalk.white(
+      "> ",
+    )}`,
+  });
+  rl.prompt();
 
-  return prompt(input);
-};
-
-const repl = async () => {
-  try {
-    const { INPUT } = await getInput();
-
-    if (INPUT.trim()) {
-      console.log(chalk.yellow(evaluate(INPUT)));
+  rl.on("line", (line) => {
+    if (line.trim() === "") {
+      return rl.prompt();
     }
-  } catch (e) {
-    console.error(e);
-  }
-
-  repl();
+    switch (line.trim()) {
+      case ".end":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case ".q":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case ".quit":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case "quit":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case ".exit":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case "exit":
+        console.log("Have a nice day!");
+        process.exit(0);
+      case ".help":
+        console.log(
+          chalk.cyan(
+            "Welcome to Arith - a simple, Lisp-like programming language.\n",
+          ),
+        );
+        console.log(`You are using Arith ${version}`);
+        console.log(chalk.blue("Here are the valid commands:\n"));
+        console.log("COMMAND", "                DESCRIPTION");
+        console.log(
+          "<none>, i",
+          "              Open the interpreter in interactive/REPL mode",
+        );
+        console.log(
+          "help",
+          "                   Print this help message",
+        );
+        console.log(
+          "version",
+          "                Print the version of Arith you're using",
+        );
+        console.log(
+          "run",
+          "                    Execute the contents of a valid Arith file",
+        );
+        console.log(); // blank line
+        console.log(
+          chalk.cyan(
+            "Use the command 'arc <file>' to transpile its contents to JavaScript.\n",
+          ),
+        );
+        console.log("Enjoy!");
+        break;
+      case "help":
+        console.log(
+          chalk.cyan(
+            "Welcome to Arith - a simple, Lisp-like programming language.\n",
+          ),
+        );
+        console.log(`You are using Arith ${version}`);
+        console.log(chalk.blue("Here are the valid commands:\n"));
+        console.log("COMMAND", "                DESCRIPTION");
+        console.log(
+          "<none>, i",
+          "              Open the interpreter in interactive/REPL mode",
+        );
+        console.log(
+          "help",
+          "                   Print this help message",
+        );
+        console.log(
+          "version",
+          "                Print the version of Arith you're using",
+        );
+        console.log(
+          "run",
+          "                    Execute the contents of a valid Arith file",
+        );
+        console.log(); // blank line
+        console.log(
+          chalk.cyan(
+            "Use the command 'arc <file>' to transpile its contents to JavaScript.\n",
+          ),
+        );
+        console.log("Enjoy!");
+        break;
+      default:
+        try {
+          console.log(chalk.yellow(evaluate(line.trim())));
+        } catch (err) {
+          console.error(err);
+        }
+        break;
+    }
+    rl.prompt();
+  }).on("close", () => {
+    console.log("Have a nice day!");
+    process.exit(0);
+  });
 };
 
 if (require.main === module) {

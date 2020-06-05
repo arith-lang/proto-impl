181aa0ab89554b595e05640597927199a13d979d
Amy Shackles <amyshackles@gmail.com>
2020-06-04 17:26:39 -0700

    Removed interactive help description as it wouldn't be usable in-repl

--- src/help.js
+++ src/help.js
@@ -1,5 +1,4 @@
 let help = [
-  ["<none>, i", "Open the interpreter in interactive/REPL mode"],
   [".editor", "Enter editor mode, to allow multi-line expressions"],
   [".exit", "Exit the Arith REPL"],
   [".help", "Display the valid commands for Arith"],

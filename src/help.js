0b6399533ebc22f51ce44c4dfec42fe3a56b3710
Amy Shackles <amyshackles@gmail.com>
2020-06-04 17:22:00 -0700

    Porting over to repl

---
+++ src/help.js
@@ -0,0 +1,9 @@
+let help = [
+  ["<none>, i", "Open the interpreter in interactive/REPL mode"],
+  [".editor", "Enter editor mode, to allow multi-line expressions"],
+  [".exit", "Exit the Arith REPL"],
+  [".help", "Display the valid commands for Arith"],
+  [".load", "Load a file"],
+  [".version", "Print the version of Arith you're using"],
+];
+module.exports = { help };
--- src/repl.js
+++ src/repl.js
@@ -3,36 +3,65 @@ const path = require("path");
 const { prompt } = require("inquirer");
 const chalk = require("chalk");
 const { evaluate } = require("./evaluate");
+const repl = require("repl");
+const { help } = require("./help");
 
 const version = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../package.json"), "utf-8"),
 ).version;
 
-const getInput = () => {
-  const input = [
-    {
-      name: "INPUT",
-      type: "input",
-      prefix: chalk.green(`(arith v${version}):`),
-      message: ">",
-    },
-  ];
-
-  return prompt(input);
+const eval = (cmd, context, filename, callback) => {
+  callback(null, evaluate(cmd));
 };
 
-const repl = async () => {
-  try {
-    const { INPUT } = await getInput();
-
-    if (INPUT.trim()) {
-      console.log(chalk.yellow(evaluate(INPUT)));
-    }
-  } catch (e) {
-    console.error(e);
-  }
-
-  repl();
+const initializeRepl = () => {
+  const replServer = repl.start({
+    prompt: `${chalk.green(`(arith v${version})`)}: ${chalk.white(
+      "> ",
+    )}`,
+    input: process.stdin,
+    output: process.stdout,
+    eval: eval,
+  });
+  replServer.on("exit", () => {
+    console.log("Have a nice day!");
+    process.exit();
+  });
+  replServer.defineCommand("version", {
+    help: "Displays the current version of Arith",
+    action(name) {
+      this.clearBufferedCommand();
+      console.log(`Arith version ${version}`);
+      this.displayPrompt();
+    },
+  });
+  replServer.defineCommand("help", {
+    help: "Displays the valid commands in Arith",
+    action(name) {
+      this.clearBufferedCommand();
+      console.log(
+        chalk.cyan(
+          "Welcome to Arith - a simple, Lisp-like programming language.\n",
+        ),
+      );
+      console.log(`You are using Arith ${version}`);
+      console.log(chalk.blue("Here are the valid commands:\n"));
+      console.log("COMMAND", "                DESCRIPTION");
+      help.map(([command, description]) =>
+        command.length >= 6
+          ? console.log(command, `\t\t${description}`)
+          : console.log(command, `\t\t\t${description}`),
+      );
+      console.log(); // blank line
+      console.log(
+        chalk.cyan(
+          "Use the command 'arc <file>' to transpile its contents to JavaScript.\n",
+        ),
+      );
+      console.log("Enjoy!");
+      this.displayPrompt();
+    },
+  });
 };
 
 if (require.main === module) {
@@ -41,7 +70,7 @@ if (require.main === module) {
       `*** Welcome to the Arith programming language, v${version} ***`,
     ),
   );
-  repl();
+  initializeRepl();
 }
 
-module.exports = { repl };
+module.exports = { initializeRepl };

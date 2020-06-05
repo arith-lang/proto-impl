63739764076dde1648f0407e64e15d61b0897416
Amy Shackles <amyshackles@gmail.com>
2020-06-05 16:11:38 -0700

    Adds the .save command to help

--- src/help.js
+++ src/help.js
@@ -3,6 +3,7 @@ let help = [
   [".exit", "Exit the Arith REPL"],
   [".help", "Display the valid commands for Arith"],
   [".load", "Load a file"],
+  [".save <path/to/file>", "Save a REPL session as a file"],
   [".version", "Print the version of Arith you're using"],
 ];
 module.exports = { help };

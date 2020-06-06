32e63c91ff79fff8d0a874135614ce8be76d0836
Amy Shackles <amyshackles@gmail.com>
2020-06-05 18:38:27 -0700

    Delete help.js

--- src/help.js
+++
@@ -1,9 +0,0 @@
-let help = [
-  [".editor", "Enter editor mode, to allow multi-line expressions"],
-  [".exit", "Exit the Arith REPL"],
-  [".help", "Display the valid commands for Arith"],
-  [".load", "Load a file"],
-  [".save <path/to/file>", "Save a REPL session as a file"],
-  [".version", "Print the version of Arith you're using"],
-];
-module.exports = { help };
